5ddfd84d33b90c6cbfaec60ac22b76c581020007
Aleksandr Babak <surkovich@gmail.com>
2022-10-23 22:37:04 +0900

    cash_on_delivery validation tested

--- Readme.md
+++ Readme.md
@@ -3,7 +3,9 @@ Test project for anyX job interview.
 Simple project, try-out of GraphQL, ktor and koin - completely new technologies for me.
 
 
-For this moment - no tests, only dummy persistence added, 
-some cleanup needed.
+For this moment 
+ - only some validations are test-covered, 
+ - only dummy persistence added, 
+ - some cleanup needed.
 
 
--- src/test/kotlin/com/marmotta/anyx/graphql/schema/validator/PaymentValidationTest.kt
+++ src/test/kotlin/com/marmotta/anyx/graphql/schema/validator/PaymentValidationTest.kt
@@ -3,8 +3,12 @@ package com.marmotta.anyx.graphql.schema.validator
 import com.marmotta.anyx.graphql.schema.models.AdditionalDetails
 import com.marmotta.anyx.graphql.schema.models.PaymentRegistrationDTO
 import com.marmotta.anyx.graphql.schema.validator.rule.ValidationResult
+import com.marmotta.anyx.graphql.shared.model.Courier
 import com.marmotta.anyx.graphql.shared.model.PaymentMethod
+import io.kotest.matchers.should
 import io.kotest.matchers.shouldBe
+import io.kotest.matchers.types.beInstanceOf
+import io.kotest.matchers.types.shouldBeSameInstanceAs
 import io.ktor.util.reflect.*
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
@@ -24,10 +28,57 @@ internal class PaymentValidationTest{
             additionalItem = AdditionalDetails()
         )
 
-        PaymentValidation.of(payment) shouldBe instanceOf(ValidationResult.Errors::class)
+        PaymentValidation.of(payment) should beInstanceOf<PaymentValidationDetails.Errors>()
     }
 
-//    @Test
-//    fun
+    @Test
+    fun `Cash on delivery should not accept price modifier beyond bounds`() {
+        listOf(BigDecimal(0.89), BigDecimal(1.1))
+            .forEach {
+                val payment = PaymentRegistrationDTO(
+                    customerId = 1L,
+                    price = BigDecimal(100),
+                    priceModifier = it,
+                    paymentMethod = PaymentMethod.CASH_ON_DELIVERY,
+                    dateTime = OffsetDateTime.MIN,
+                    additionalItem = AdditionalDetails()
+                )
+                PaymentValidation.of(payment) should beInstanceOf<PaymentValidationDetails.Errors>()
+            }
+    }
+
+    @Test
+    fun `Cash on delivery should fail validation on couriers beyond restricted`(){
+        Courier.values().filter { !setOf(Courier.SAGAWA, Courier.YAMATO).contains(it) }
+            .forEach {
+                val payment = PaymentRegistrationDTO(
+                    customerId = 1L,
+                    price = BigDecimal(100),
+                    priceModifier = BigDecimal("0.91"),
+                    paymentMethod = PaymentMethod.CASH_ON_DELIVERY,
+                    dateTime = OffsetDateTime.MIN,
+                    additionalItem = AdditionalDetails(courierService = it)
+                )
+                PaymentValidation.of(payment) should beInstanceOf<PaymentValidationDetails.Errors>()
+            }
+    }
+
+    @Test
+    fun `Cash on delivery with correct delivery and price modifier should pass validation`() {
+        listOf(Courier.SAGAWA, Courier.YAMATO)
+            .forEach {
+                val payment = PaymentRegistrationDTO(
+                    customerId = 1L,
+                    price = BigDecimal(100),
+                    priceModifier = BigDecimal("0.91"),
+                    paymentMethod = PaymentMethod.CASH_ON_DELIVERY,
+                    dateTime = OffsetDateTime.MIN,
+                    additionalItem = AdditionalDetails(courierService = it)
+                )
+                PaymentValidation.of(payment) should beInstanceOf<PaymentValidationDetails.Valid>()
+            }
+    }
+
+
 
 }
